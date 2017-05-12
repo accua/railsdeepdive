0038a7ba53ed4655b279346097fd7f6617284ae1
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-05-12 14:53:32 -0700

    integrated new and create actions in portfolio controller.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,20 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    @portfolio_item.save
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
 end

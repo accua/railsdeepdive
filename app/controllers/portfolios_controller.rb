35f488346dd7cb1f5fe7e6a296f24b65d368f62b
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-05-12 16:10:02 -0700

    integrated edit and update commands on portfolio controller.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,7 +10,27 @@ class PortfoliosController < ApplicationController
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
-    @portfolio_item.save
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+    else
+      format.html { render :edit }
+    end
   end
 
   private

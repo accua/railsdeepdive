384db5fcc533ec4219f01edc2d3712c1b8cf4e10
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-05-13 20:38:18 -0700

    Integrated friendly id gem into blogs, and added enum status to blog as well.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about' to: 'pages#about'
-  get 'contact' to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
 
